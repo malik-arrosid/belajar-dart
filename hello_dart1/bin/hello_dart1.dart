1e11bec814bc15632634d3044c60885848e39622
malik-arrosid <malikarrosid.md@gmail.com>
2024-07-27 16:06:09 +0900

    first commit

---
+++ hello_dart1/bin/hello_dart1.dart
@@ -0,0 +1,5 @@
+import 'package:hello_dart1/hello_dart1.dart' as hello_dart1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hello_dart1.calculate()}!');
+}
---
+++ hello_dart1/test/hello_dart1_test.dart
@@ -0,0 +1,8 @@
+import 'package:hello_dart1/hello_dart1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
